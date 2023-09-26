5f165650633c1871cf8d88f934f55ace2cfd449b
Patrícia Peres [1222641] <1222641@isep.ipp.pt>
2023-09-26 22:37:41 +0100

    new tests in calc.test.ts and some updates on calc.ts

--- src/domain/calc.ts
+++ src/domain/calc.ts
@@ -1,3 +1,9 @@
+/**
+ * This is a simple sum function that returns the sum of two numbers.
+ * 
+ * @param parcelas 
+ * @returns 
+ */
 
 export function sumOnly(...parcelas: number[]): number {
     return parcelas.reduce((total, parcela) => total + parcela);
