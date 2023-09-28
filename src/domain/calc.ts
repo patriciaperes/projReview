f8cdc42ee474a21557761b9da43919db5eaba5a9
hiksilva <1222632@isep.ipp.pt>
2023-09-28 21:24:36 +0100

    prettier formatting

--- src/domain/calc.ts
+++ src/domain/calc.ts
@@ -1,10 +1,10 @@
 /**
  * This is a simple sum function that returns the sum of two numbers.
- * 
- * @param parcelas 
- * @returns 
+ *
+ * @param parcelas
+ * @returns
  */
 
 export function sumOnly(...parcelas: number[]): number {
-    return parcelas.reduce((total, parcela) => total + parcela);
-} 
+  return parcelas.reduce((total, parcela) => total + parcela);
+}
--- src/test/calc.test.ts
+++ src/test/calc.test.ts
@@ -1,26 +1,22 @@
-
 import { sumOnly } from "../domain/calc";
 
 /**
  * Suite of tests for the sumOnly function.
  */
 
-
 describe("sumOnly", () => {
+  test("verifica resultado", () => {
+    let result = sumOnly(1, 2, 3, 4, 5);
+    expect(result).toBe(15);
+  });
 
-    test("verifica resultado", () => {
-        let result = sumOnly(1, 2, 3, 4, 5);
-        expect(result).toBe(15);
-    });
+  test("verifica resultado com 1 parcela", () => {
+    let result = sumOnly(1);
+    expect(result).toBe(1);
+  });
 
-    test("verifica resultado com 1 parcela", () => {
-        let result = sumOnly(1);
-        expect(result).toBe(1);
-    });
-
-    test("verifica resultado com 2 parcelas", () => {
-        let result = sumOnly(7, 10);
-        expect(result).toBe(17);
-    });
-
+  test("verifica resultado com 2 parcelas", () => {
+    let result = sumOnly(7, 10);
+    expect(result).toBe(17);
+  });
 });
